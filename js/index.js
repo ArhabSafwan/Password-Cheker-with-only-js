ef993688a8ee322031d748b5da83b37cad2ecb27
joan-tomas-1995 <joan.tomas.miralles@gmail.com>
2023-04-08 13:36:45 +0200

    I have changed the style, now the texts are displayed in the html not in the console, it is evaluated every time the text is entered  in the input with the "input" method.

--- js/index.js
+++ js/index.js
@@ -1,47 +1,50 @@
-function containSymbol(str) //bring all the symbol
-{
-    const symbol = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-    return symbol.test(str);
+let inputPassword = document.querySelector(".input-password");
+let textOutput = document.querySelector(".text-output");
+
+//CHECK PASSWORD ON INPUT
+inputPassword.addEventListener("input", function () {
+  let str = inputPassword.value;
+
+  let hasSymbol = containSymbol(str);
+  let hasUpper = isUpper(str);
+  let hasNumber = num(str);
+  let hasLower = isLower(str);
+
+  if (str == "") {
+    textOutput.innerHTML = "Fill the password";
+  } else if (str.length <= 8) {
+    textOutput.innerHTML = "password is too short";
+  } else if (!hasSymbol) {
+    textOutput.innerHTML = "password must have a special char";
+  } else if (!hasUpper) {
+    textOutput.innerHTML = "password need a upper case letter";
+  } else if (!hasNumber) {
+    textOutput.innerHTML = "password must contain a numerical value";
+  } else if (!hasLower) {
+    textOutput.innerHTML = "password must contain a lower case";
+  } else {
+    textOutput.innerHTML = "password is strong";
+  }
+});
+
+// ALL FUNCTIONS
+
+function containSymbol(str) {
+  //bring all the symbol
+  const symbol = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  return symbol.test(str);
 }
 
-function isUpper(str) {          //calling regex
-    return /[A-Z]/.test(str);
+function isUpper(str) {
+  //calling regex
+  return /[A-Z]/.test(str);
 }
 
-function num(str){              //calling regex
-    return /[0-9]/.test(str);
+function num(str) {
+  //calling regex
+  return /[0-9]/.test(str);
 }
 
-function isLower(str){
-    return /[a-z]/.test(str);
-}
-
-var str = prompt("please enter your passa");
-
-if(str == ''){
-    
-console.log('Fill the password');
-}
-
-else if (str.length <= 8)
-{
-    console.log('password is too short');
-}
-else if(!containSymbol(str))
-{
-    console.log('password must have a special char');
-}
-else if(!isUpper(str)){
-console.log('pass need a upper case letter');
-}
-else if(!num(str)){
-console.log('pass must contain a numerical value');
-}
-else if(!isLower(str)){
-   console.log('pass must contain a lower case');
-    }
-
-else 
-{
-    console.log("passa is strong");
+function isLower(str) {
+  return /[a-z]/.test(str);
 }
